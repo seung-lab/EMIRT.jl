720b7da902f2a9b7ff20beccff147a8f30483e53
jingpengwu <jingpeng.wu@gmail.com>
2016-01-20 11:08:00 -0500

    transpose the random_color_show image to match python plot

--- src/emshow.jl
+++ src/emshow.jl
@@ -4,6 +4,13 @@ using PyCall
 @pyimport emirt.show as emshow
 
 function random_color_show( seg )
+    if ndims(seg)==3
+        @assert size(seg,3) == 1
+        seg = seg[:,:,1]
+    end
     @assert ndims(seg) == 2
-    emshow.random_color_show( seg )
+
+    # note that the seg was transposed to be consistent with python plot
+    # because julia use column major and python use row major
+    emshow.random_color_show( seg' )
 end
