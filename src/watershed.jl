9e9e372119517efbc85cdbab4b99899a338dab4c
jingpengwu <jingpeng.wu@gmail.com>
2016-02-16 14:51:20 -0500

    wsseg2d did not modified the segmentation! mergert! seems did not modify the seg in place.

--- src/watershed.jl
+++ src/watershed.jl
@@ -2,15 +2,6 @@ using Watershed
 
 export wsseg2d, watershed, mergert!, mergert
 
-function wsseg2d(affs, low=0.3, high=0.9, thresholds=[(256,0.3)], dust_size=100, thd_rt=0.5)
-    seg = zeros(UInt32, size(affs)[1:3] )
-    for z in 1:size(affs,3)
-        seg[:,:,z], rt = watershed(affs[:,:,z,:], low, high, thresholds, dust_size)
-        mergert!(seg[:,:,z], rt, thd_rt)
-    end
-    return seg
-end
-
 function watershed(affs, low=0.3, high=0.9, thresholds=[(256, 0.3)], dust_size=100)
     sag = steepestascent(affs, low, high)
     divideplateaus!(sag)
@@ -32,47 +23,60 @@ function mergert!(seg, rt, thd=0.5)
     # get the ralative parent dict
     pd = Dict()
     # initialized as children and parents
+    num = 0
     for t in rt
         a, c, p = t
         @assert p>0 && c>0
+        if a >= thd
+            num = num + 1
+        end
         pd[c] = (p, a)
     end
+    println("number of merging edges: $num")
+    println("total number: $(length(rt))")
 
     # get the relative root id
-    # dictionary of root id
+    # root dict
     rd = Dict()
+    # root set
+    rset = IntSet()
     for t in rt
         # get affinity and segment IDs of parent and child
-        a, c, p = t
-
-        # find the real root
-        path = []
-        while a >= thd && p!=c
-            # record path for path compression
-            push!(path, c)
-            # reset the child as parent
-            c = p
+        a, c0, p0 = t
+        c = c0
+        p = p0
+        while a >= thd && haskey(pd,p)
             # next pair of child and parent
-            if haskey(pd, p)
-                a = pd[p][2]
-            else
-                break
-            end
+            a = pd[p][2]
+            p = pd[p][1]
         end
-        # path compression
-        for n in path
-            rd[n] = (p, a)
+        if p != p0
+            rd[c0] = p
+            push!(rset, p)
         end
     end
+    println("number of trees: $(length(rset))")
 
     #println("root dict: $rd")
+    num = 0
     # set the segment id as relative root id
     for i in eachindex(seg)
-        v = seg[i]
-        if haskey(rd, v)
-            root = rd[v][1]
-            println("root: $(UInt64(root))")
-            seg[i] = root
+        # segment id
+        sid = seg[i]
+        if haskey(rd, sid)
+            #println("root: $(UInt64(root))")
+            seg[i] = rd[sid]
+            num = num + 1
         end
     end
+    println("really merged edges: $num")
+end
+
+function wsseg2d(affs, low=0.3, high=0.9, thresholds=[(256,0.3)], dust_size=100, thd_rt=0.5)
+    seg = zeros(UInt32, size(affs)[1:3] )
+    for z in 1:size(affs,3)
+        seg[:,:,z], rt = watershed(affs[:,:,z,:], low, high, thresholds, dust_size)
+        seg[:,:,z] = mergert(seg[:,:,z], rt, thd_rt)
+    end
+    return seg
 end
