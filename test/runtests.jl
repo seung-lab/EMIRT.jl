971ab611e5816ede22472420fa8e5f9a53d5082e
jingpengwu <jingpeng.wu@gmail.com>
2016-03-28 16:03:23 -0400

    add test for singletons

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,3 +9,8 @@ lbl = Array{UInt32,3}(lbl)
 @time re, rem, res, rf, rfm, rfs = segerror(lbl,lbl)
 @show re, rem, res, rf, rfm, rfs
 @assert abs(rf-1) < 0.01
+
+seg = Array{UInt32,3}(reshape(range(1,length(lbl)), size(lbl)))
+@time re, rem, res, rf, rfm, rfs = segerror(seg,lbl)
+@show re, rem, res, rf, rfm, rfs
+@assert abs(rf-0) < 0.01
